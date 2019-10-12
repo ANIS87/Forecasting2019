09baf63fec35492a2a6064c8db19d944eb18a79e
root <root@krayni-Latitude-E6410>
2019-10-12 14:11:04 +0200

    mise a jour3

--- Weather_orders_java_maven/src/main/java/com/mycompany/app/App.java
+++ Weather_orders_java_maven/src/main/java/com/mycompany/app/App.java
@@ -1,4 +1,5 @@
 package com.mycompany.app;
+
 //necessary components are imported
 import java.io.BufferedReader;
 import java.io.File;
@@ -27,59 +28,40 @@ import org.json.JSONException;
 
 import com.mycompany.model.Order;
 import com.mycompany.model.Orders;
-import com.mycompany.utils.ParseXmlFile;
+import com.mycompany.utils.ProcessXmlData;
 import com.mycompany.utils.WriteInXml;
 import com.thoughtworks.xstream.XStream;
-/**
- * Hello world!
- *
- */
 
+public class App {
+	public static void helpme() {
 
-public class App 
-{
-    public static void helpme()
-    {
-    	
-    	String message=" Pour utiliser ce module, il faut indiquer le fichier xml d'entree et le fichier de sortie ,avec options -i et -o \n "
-    			+ "par exemple java -jar my-app-1.0-SNAPSHOT.jar -i testinput.xml -o testoutput.xml";
-    	System.out.println(message);
-    }
-	
-    public static void main( String[] args )throws IOException, URISyntaxException, JAXBException{
-    	String filename="";
-    	String outputfile="";
-    	
-    	if(args.length==4)
-    	{
-           for(int i=0;i<4;i++)
-             {
-    	       if(args[i].equalsIgnoreCase("-i") ||args[i].equalsIgnoreCase("-input") )
-    	         {
-                filename=args[i+1];
-    	          }
-    	          if(args[i].equalsIgnoreCase("-o") ||args[i].equalsIgnoreCase("-output") )
-    	        	 outputfile=args[i+1];
-                   }
-    	}
-    	
-    	if(args.length<4 || filename.isEmpty())
-    	{
-    		helpme();	
-    	}
-    	else
-    	{
-        
-    	ParseXmlFile parsexml=new ParseXmlFile();
-    	WriteInXml writeinxml=new WriteInXml();
-    	Map<String,String> result=ParseXmlFile.parsefile(filename,outputfile);
-        GetResponseApi test=new GetResponseApi();
-        //test.getdata(result);
-    	}
-    	
-      
-       
-       
-    }
+		String message = " Pour utiliser ce module, il faut indiquer le fichier xml d'entree et le fichier de sortie ,avec options -i et -o \n "
+				+ "par exemple java -jar my-app-1.0-SNAPSHOT.jar -i testinput.xml -o testoutput.xml";
+		System.out.println(message);
+	}
+
+	public static void main(String[] args) throws IOException, URISyntaxException, JAXBException {
+		String filename = "";
+		String outputfile = "";
+
+		if (args.length == 4) {
+			for (int i = 0; i < 4; i++) {
+				if (args[i].equalsIgnoreCase("-i") || args[i].equalsIgnoreCase("-input")) {
+					filename = args[i + 1];
+				}
+				if (args[i].equalsIgnoreCase("-o") || args[i].equalsIgnoreCase("-output"))
+					outputfile = args[i + 1];
+			}
+		}
+
+		if (args.length < 4 || filename.isEmpty()) {
+			helpme();
+		} else {
+
+					
+			 ProcessXmlData.parsefile(filename, outputfile);
+			
+		}
+
+	}
 }
-
